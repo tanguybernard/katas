fb1b109476f0f8e3e00fb8b4558520bb092b593f
tanguybernard <bernardtanguy1pro@gmail.com>
2023-04-06 11:42:56 +0200

    gildedrose kata, create some tests

---
+++ gildedrose-refactoring-kotlin/build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	kotlin("jvm") version "1.8.10"
+}
+
+group = "com.gildedrose"
+version = "1.0-SNAPSHOT"
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation(kotlin("stdlib"))
+	testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+	testImplementation("org.assertj:assertj-core:3.16.1")
+}
+
+tasks.test {
+	useJUnitPlatform()
+	testLogging {
+		events("passed", "skipped", "failed")
+	}
+}
+
+// config JVM target to 1.8 for kotlin compilation tasks
+tasks.withType<KotlinCompile>().configureEach {
+	kotlinOptions.jvmTarget = "1.8"
+}
---
+++ gildedrose-refactoring-kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -0,0 +1,187 @@
+package com.gildedrose
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class GildedRoseTest {
+
+    @Test
+    fun foo() {
+        val items = listOf(Item("foo", 0, 0))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals("foo", app.items[0].name)
+
+    }
+
+    @Test
+    fun `At the end of each day our system lowers both values for every item`() {
+
+        val items = listOf(
+            Item("+5 Dexterity Vest", 10, 20),
+            Item("Elixir of the Mongoose", 5, 7)
+        )
+
+        val app = GildedRose(items)
+        app.updateQuality()
+            assertThat(listOf(
+            Item("+5 Dexterity Vest", 9, 19),
+            Item("Elixir of the Mongoose", 4, 6)
+        )).usingRecursiveComparison().ignoringCollectionOrder().isEqualTo(app.items)
+
+    }
+
+    @Test
+    fun `Once the sell by date has passed, Quality degrades twice as fast`() {
+
+        val items = listOf(
+            Item("+5 Dexterity Vest", 0, 4),
+            Item("Elixir of the Mongoose", -1, 7)
+        )
+
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertThat(listOf(
+            Item("+5 Dexterity Vest", -1, 2),
+            Item("Elixir of the Mongoose", -2, 5)
+        )).usingRecursiveComparison().ignoringCollectionOrder().isEqualTo(app.items)
+
+    }
+
+    @Test
+    fun `Aged Brie actually increases in Quality the older it gets`(){
+        val items = listOf(
+            Item("Aged Brie", 1, 4),
+        )
+
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertThat(listOf(
+            Item("Aged Brie", 0, 5),
+        )).usingRecursiveComparison().ignoringCollectionOrder().isEqualTo(app.items)
+    }
+
+    @Test
+    fun `The Quality of an item is never more than 50`(){
+        val items = listOf(
+            Item("Aged Brie", 3, 50),
+        )
+
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertThat(listOf(
+            Item("Aged Brie", 2, 50),
+        )).usingRecursiveComparison().ignoringCollectionOrder().isEqualTo(app.items)
+    }
+
+    @Test
+    fun `The Quality of an item is never more than 50, case 51`(){
+        //TODO Maybe the code must prevent the creation of item more than 50 quality ?
+
+        val items = listOf(
+            Item("Aged Brie", 3, 51),
+        )
+
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertThat(listOf(
+            Item("Aged Brie", 2, 51),
+        )).usingRecursiveComparison().ignoringCollectionOrder().isEqualTo(app.items)
+    }
+
+
+    @Test
+    fun `Sulfuras, being a legendary item, never has to be sold or decreases in Quality`(){
+
+        val items = listOf(
+            Item("Sulfuras, Hand of Ragnaros", 3, 40),
+        )
+
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertThat(listOf(
+            Item("Sulfuras, Hand of Ragnaros", 3, 40),
+        )).usingRecursiveComparison().ignoringCollectionOrder().isEqualTo(app.items)
+    }
+
+    @Test
+    fun `Backstage passes, Quality increases by 2 when there are 10 days or less`(){
+
+        val items = listOf(
+            Item("Backstage passes to a TAFKAL80ETC concert", 9, 40),
+        )
+
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertThat(listOf(
+            Item("Backstage passes to a TAFKAL80ETC concert", 8, 42),
+        )).usingRecursiveComparison().ignoringCollectionOrder().isEqualTo(app.items)
+    }
+
+    @Test
+    fun `Backstage passes, Quality increases by 3 when there are 5 days or less`(){
+
+        val items = listOf(
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 40),
+        )
+
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertThat(listOf(
+            Item("Backstage passes to a TAFKAL80ETC concert", 4, 43),
+        )).usingRecursiveComparison().ignoringCollectionOrder().isEqualTo(app.items)
+    }
+
+    @Test
+    fun `Backstage passes, Quality drops to 0 after the concert`(){
+
+        val items = listOf(
+            Item("Backstage passes to a TAFKAL80ETC concert", 0, 40),
+        )
+
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertThat(listOf(
+            Item("Backstage passes to a TAFKAL80ETC concert", -1, 0),
+        )).usingRecursiveComparison().ignoringCollectionOrder().isEqualTo(app.items)
+    }
+
+    @Test
+    fun testGivenExample(){
+
+        val items = listOf(
+            Item("+5 Dexterity Vest", 10, 20), //
+            Item("Aged Brie", 2, 0), //
+            Item("Elixir of the Mongoose", 5, 7), //
+            Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+            Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            //TODO this conjured item does not work properly yet
+            //Item("Conjured Mana Cake", 3, 6)
+
+        )
+
+
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertThat(listOf(
+            Item("+5 Dexterity Vest", 9, 19), //
+            Item("Aged Brie", 1, 1), //
+            Item("Elixir of the Mongoose", 4, 6), //
+            Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+            Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            Item("Backstage passes to a TAFKAL80ETC concert", 14, 21),
+            Item("Backstage passes to a TAFKAL80ETC concert", 9, 50),
+            Item("Backstage passes to a TAFKAL80ETC concert", 4, 50),
+        )).usingRecursiveComparison().isEqualTo(app.items)
+
+    }
+
+
+
+}
+
+
