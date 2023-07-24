c746e4dab8f286ae849f9befdde0aace1c651ec1
tanguybernard <bernardtanguy1pro@gmail.com>
2023-07-24 10:47:56 +0200

    movie rental kata: extract method to get amount

--- movie-rental-typescript/src/customer.ts
+++ movie-rental-typescript/src/customer.ts
@@ -24,26 +24,8 @@ export class Customer {
         let movies = [];
 
         for (const each of this.rentals) {
-            let thisAmount = 0;
 
-            // determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2) {
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    }
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3) {
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    }
-                    break;
-            }
+            const thisAmount = this.getAmount(each);
 
             // add frequent renter points
             frequentRenterPoints++;
@@ -59,6 +41,29 @@ export class Customer {
 
         return this.formatString(this.getName(), movies, totalAmount.toFixed(1), frequentRenterPoints)
 
+    }
+
+    private getAmount(rental: Rental) {
+        let amount = 0;
+        // determine amounts for each line
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (rental.getDaysRented() > 2) {
+                    amount += (rental.getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                amount += rental.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                amount += 1.5;
+                if (rental.getDaysRented() > 3) {
+                    amount += (rental.getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return amount;
     }
 
     formatString(name: String, movies: {title: string, amount: string}[], totalAmount: string, frequentRenterPoints:number): string{
